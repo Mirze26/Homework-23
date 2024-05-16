200bbb507ceaea48b1fee131aaa73ed6da5a549b
Mirze26 <mirzaab@code.edu.az>
2024-05-17 00:31:36 +0400

    home work

---
+++ Palindrome js/src/js/script.js
@@ -0,0 +1,39 @@
+
+//task 1 --------------------------------------------------
+
+function bigLetter(str) {
+    return text.replace(/\b\w/g, function(str) {
+        return str.toUpperCase();
+    });
+}
+
+let text = "boyuy herifler cumlesi";
+console.log(bigLetter(text));
+
+//---------------------------------------------------------
+
+
+
+//task 2 ---------------------------------------------------
+
+function isPalindrome(str) {
+    let text = str.length - 1;
+    
+
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] == str[text]) {
+            return true;
+        }
+        else if(text[i] == str[i].toUpperCase){
+            return true;
+        }
+        text ++;
+    }
+    return false;
+}
+
+let word = "Ata";
+console.log(isPalindrome(word));
+
+//----------------------------------------------------------------
+
